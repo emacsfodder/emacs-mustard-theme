d03f9a01036b2681b162cd3fa2c0940b8bcd76ee
Jasonm23 <jasonm23@gmail.com>
2014-11-16 14:30:27 +0800

    Update author and version keys

--- mustard-theme.el
+++ mustard-theme.el
@@ -1,8 +1,8 @@
 ;;; mustard-theme.el --- an Emacs 24 theme based on Mustard (tmTheme)
 ;;
-;;; Author: Auto Converted to Emacs 24 by tmtheme-to-deftheme (tm2deftheme)
-;;; Version: 1416104799
-;;; Original author: 
+;;; Author: Jason Milkins
+;;; Version: 20141116
+ 
 ;;; Url: https://github.com/emacsfodder/tmtheme-to-deftheme
 ;;; Package-Requires: ((emacs "24.0"))
 ;;
